dde8d8b4d832d5d2f03a93ad09f4924a955dd6aa
simarchawla1 <65342780+simarchawla1@users.noreply.github.com>
2020-06-22 23:15:44 +0530

    Add files via upload

---
+++ speakhello.js
@@ -0,0 +1,15 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+(function(window){
+  var helloSpeaker ={};
+   var speakWord = "Hello";
+   helloSpeaker.speak= function(name){
+     console.log(speakWord + " " + name);
+      };
+      window.helloSpeaker= helloSpeaker;
+})(window);
+
+
